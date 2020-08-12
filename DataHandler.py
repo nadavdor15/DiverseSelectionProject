0661924730edf42a810b2a8af4d02d1e9ba0be4c
nadavdor15 <nadavdor15@gmail.com>
2020-08-12 05:17:48 +0300

    DataHandler extracts user ids and hotel ids

--- DataHandler.py
+++ DataHandler.py
@@ -1,3 +1,13 @@
+import pandas as pd
+
+
 class DataHandler(object):
-    def __init__(self):
-        pass
+    def __init__(self, businesses_filename, review_filename):
+        hotels_data = pd.read_json(businesses_filename, lines=True)
+        self.hotels = hotels_data['id'].unique().tolist()
+        self.hotels = list(map(str, self.hotels))
+
+        self.reviews = pd.read_json(review_filename, lines=True)
+        reviews_authors = pd.read_json(self.reviews["author"].to_json(orient='records'))
+        self.users = reviews_authors["id"].unique().tolist()
+        self.users.remove('')
--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from BaseDiversitySolver import *
+from DataHandler import *
+
+businesses_filename = 'C:\\Users\\revcr\\Downloads\\review\\offering.json'
+reviews_filename = 'C:\\Users\\revcr\\Downloads\\review\\review_1.json'
 
 
 def main():
-    users = ['Alice', 'Bob', 'Carol', 'David', 'Eve']
-    groups = [['Alice', 'David'], ['Bob'], ['Carol'], ['Eve'],
-              ['Alice', 'Carol'],
-              ['Bob'], ['Alice', 'David', 'Eve'],
-              ['Bob'], ['Eve', 'David'], ['Alice'],
-              ['Alice'], ['Carol', 'Eve'], ['Bob'],
-              ['Carol', 'Eve'], ['Alice'], ['Bob']]
+    # running example
+    # users = ['Alice', 'Bob', 'Carol', 'David', 'Eve']
+    # groups = [['Alice', 'David'], ['Bob'], ['Carol'], ['Eve'],
+    #           ['Alice', 'Carol'],
+    #           ['Bob'], ['Alice', 'David', 'Eve'],
+    #           ['Bob'], ['Eve', 'David'], ['Alice'],
+    #           ['Alice'], ['Carol', 'Eve'], ['Bob'],
+    #           ['Carol', 'Eve'], ['Alice'], ['Bob']]
+
+    data_handler = DataHandler(businesses_filename, reviews_filename)
+    users = data_handler.users
+    groups = users
     solver = BaseDiversitySolver(users, groups)
     print(solver.solve(bucket_size=2, weight='LBS', cover='Single'))
 
