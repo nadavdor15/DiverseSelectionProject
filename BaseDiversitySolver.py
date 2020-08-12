725053419ebb252cc605781d7a7af8d27aeddb09
nadavdor15 <nadavdor15@gmail.com>
2020-08-12 03:47:51 +0300

    A minor change of code structure

--- BaseDiversitySolver.py
+++ BaseDiversitySolver.py
@@ -7,6 +7,7 @@ Group = recordclass('Group', 'id wei cov users')
 
 class BaseDiversitySolver(object):
     def __init__(self, users, groups):
+        self.solution = []
         self.users = [User(user, None, []) for user in users]
         self.groups = [Group(i, None, None, []) for i in range(len(groups))]
         for user in self.users:
@@ -16,10 +17,12 @@ class BaseDiversitySolver(object):
                     user.groups.append(group)
 
     def solve(self, bucket_size, weight='LBS', cover='Single'):
-        solution = []
+        if len(self.solution) > 0:
+            return self.solution
+
         for group in self.groups:
-            group.wei, group.cov = self.weight(group, weight, len(solution)),\
-                                   self.cover(group, cover, len(solution))
+            group.wei, group.cov = self.weight(group, weight, bucket_size),\
+                                   self.cover(group, cover, bucket_size)
         for user in self.users:
             user.marg = BaseDiversitySolver.marg(user)
 
@@ -29,7 +32,7 @@ class BaseDiversitySolver(object):
             users_margs = [user.marg for user in self.users]
             max_user_idx = users_margs.index(max(users_margs))
             max_user = self.users[max_user_idx]
-            solution.append(max_user.id)
+            self.solution.append(max_user.id)
             self.users.remove(max_user)
             for group in self.groups:
                 if group.cov > 0 and max_user in group.users:
@@ -38,24 +41,25 @@ class BaseDiversitySolver(object):
                         for user in group.users:
                             user.marg -= group.wei
 
-        return solution
+        return self.solution
 
-    def weight(self, group, type, subset_size):
+    def weight(self, group, type, bucket_size):
         if type == 'Iden':
             return 1
         elif type == 'LBS':
             return len(group.users)
         elif type == 'EBS':
-            order = sorted(self.groups, key=lambda group: len(group.users)).index(group) + 1
-            return (subset_size + 1)**order
+            sorted_groups = sorted(self.groups, key=lambda group: len(group.users))
+            order = sorted_groups.index(group) + 1
+            return (bucket_size + 1)**order
         else:
             raise Exception('Unsupported weight function')
 
-    def cover(self, group, type, subset_size):
+    def cover(self, group, type, bucket_size):
         if type == 'Single':
             return 1
         elif type == 'Prop':
-            return max(floor((len(group.users)*subset_size)/len(self.users)),1)
+            return max(floor(bucket_size*(len(group.users))/len(self.users)),1)
         else:
             raise Exception('Unsupported cover function')
 
